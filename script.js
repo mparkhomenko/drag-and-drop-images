ea645ef30396826ccc2233b024b76d74d01e75e6
maksim <maksim.parkhomenko96@gmail.com>
2017-12-06 16:47:31 +0300

    fix

---
+++ script.js
@@ -0,0 +1,45 @@
+﻿$(document).ready(function () {
+    $('#send').on('click', function() {
+        let regExpEmail = /[a-z0-9][a-z0-9\._-]*[a-z0-9]*@([a-z0-9]+([a-z0-9-]*[a-z0-9]+)*\.)+[a-z]+/i,
+            name = $('#name').val(),
+            email = $('#email').val(),
+            message = $('#message').val(),
+            phone = $('#phone').val();
+
+        if(regExpEmail.exec(email) && name != '' && phone != '') {
+            $('#send').attr("disabled", "disabled");
+            let formData = new FormData($('form')[0]),
+                i;
+            formData.append("name", name);
+            formData.append("email", email);
+            formData.append("message", message);
+            formData.append("phone", phone);
+            for(i = 0; i < tmp.length; i++) {
+                    formData.append('file[]', tmp[i]);
+            }
+
+            console.log(tmp);
+
+            $.ajax({
+                type: "POST",
+                url: "script.php",
+                processData: false,
+                contentType: false,
+                cache: false,
+                data: formData
+            }).done(function (data) {
+                if (data == "ok") {
+                    $('#name').val(''),
+                    $('#email').val(''),
+                    $('#message').val(''),
+                    $('#phone').val('');
+                    $("#send").removeAttr("disabled");
+                } else {
+                    alert("Что-то пошло не так. Проверьте правильность введённых данных");
+                }
+            });
+        } else {
+            alert("Что-то пошло не так. Возможно вы не корректно заполнили поля");
+        }
+    });
+});
---
+++ script.php
@@ -0,0 +1,77 @@
+﻿<?php
+
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $phone = $_POST['phone'];
+        $message = $_POST['message'];
+        $message_part = "";
+
+        $reg_name = "/^\D*$/";
+        $reg_email = "/[a-z0-9][a-z0-9\._-]*[a-z0-9]*@([a-z0-9]+([a-z0-9-]*[a-z0-9]+)*\.)+[a-z]+/i";
+        // $email = "max@gmail.com";
+
+        // function removeDirectory($dir) {
+        //         if ($objs = glob($dir."*")) {
+        //                 foreach($objs as $obj) {
+        //                         is_dir($obj) ? removeDirectory($obj) : unlink($obj);
+        //                 }
+        //         }
+        //         rmdir($dir);
+        // }
+        
+        if (preg_match($reg_email, $email) && preg_match($reg_name, $name) && !empty($_FILES['file'])) {
+                $path = "/var/www/html/img/".$email."/";
+                mkdir($path);
+
+		$filePath = array();
+
+                for($i = 0; $i < count($_FILES['file']['name']); $i++) {
+                        if(!is_uploaded_file($_FILES['file']['tmp_name'][$i])) {
+                                echo 'Файл не загружен';
+                        } else {
+                                array_push($filePath, $path.$_FILES['file']['name'][$i]);
+                                move_uploaded_file($_FILES['file']['tmp_name'][$i], $filePath[$i]);
+                        }
+                }
+
+                $to = "maksim.parkhomenko96@gmail.com";
+                $subject = "Продажа битых авто";
+
+                $boundary = "--" . md5(uniqid(time()));
+                $headers = "MIME-Version: 1.0\n";
+                $headers .= "Content-Type: multipart/mixed; boundary=\"$boundary\"\n";
+                $headers .= "From: Autolom\n";
+                $multipart = "--$boundary\n";
+                $multipart .= "Content-Type: text/html; charset=utf-8\n";
+                $multipart .= "Content-Transfer-Encoding: Quot-Printed\n\n";
+                $multipart .= "<p><strong>Имя отправителя:</strong> $name</p>";
+                $multipart .= "<p><strong>E-mail:</strong> $email</p>";
+                $multipart .= "<p><strong>Телефон:</strong> $phone</p>";
+                $multipart .= "<strong>Сообщение:</strong> $message\n\n";
+
+                $message_part = '';
+
+                foreach ($filePath as $key => $value) {
+                        $tmp = explode('/', $value);
+                        $name = $tmp[count($tmp)-1];
+
+                        $fp = fopen($value, "r");
+                        $file = fread($fp, filesize($value));
+
+                        $message_part .= "--$boundary\n";
+                        $message_part .= "Content-Type: application/octet-stream\n";
+                        $message_part .= "Content-Transfer-Encoding: base64\n";
+                        $message_part .= "Content-Disposition: attachment; filename=\"$name\"\n\n";
+                        $message_part .= chunk_split(base64_encode($file)) . "\n";
+
+                        // removeDirectory($path);
+                }
+
+                $multipart .= $message_part . "--$boundary--\n";
+                if (mail ($to, $subject, $multipart, $headers)) {
+                        echo "ok";
+                }
+        } else {
+                echo "2";
+        }
+?>
